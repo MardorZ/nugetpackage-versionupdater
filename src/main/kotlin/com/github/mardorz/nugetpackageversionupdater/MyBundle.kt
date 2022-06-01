e5e8ef227ead76f87609e20f724837ce2a64c61d
GitHub Action <action@github.com>
2022-06-01 13:48:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/mardorz/nugetpackageversionupdater/MyBundle.kt
+++ src/main/kotlin/com/github/mardorz/nugetpackageversionupdater/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mardorz.nugetpackageversionupdater
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/mardorz/nugetpackageversionupdater/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mardorz.nugetpackageversionupdater.services
+
+import com.github.mardorz.nugetpackageversionupdater.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
